e3804c2a576877c4fc89d65c5b30f68e837a1628
datdt198213 <dangdat222@gmail.com>
2023-11-13 17:15:45 +0700

    .

--- simulation/script/test.py
+++ simulation/script/test.py
@@ -2,8 +2,14 @@ import os
 
 totalTime = 3600
 insertionRate = 100
-seeds = [41,42,43,44,45,46,47,48,49,50]
+seeds = [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
 numVehicles = insertionRate # number of vehicle
+timeRound = 3600
+b = 0
+e = 3600
+distance = 1000
+numVehicles = 100
+totalTime = 3600
 
 # init data of vehicle
 for seed in seeds:
@@ -16,4 +22,7 @@ for seed in seeds:
 
     # convert xml data of vehicle to 
     command = "python3 test_convert_data.py " + str(totalTime) + " " + str(seed)
+    os.system(command)
+
+    command = "node JSMinerCoin.js " + str(timeRound) + " " + str(b) + " " + str(e) + " " + str(distance) + " " + str(seed) + " " + str(numVehicles) + " " + str(totalTime)
     os.system(command)
