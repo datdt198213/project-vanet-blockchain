7c6ca99f177535ef16143f7a9bd5df809c7526cf
datdt198213 <dangdat222@gmail.com>
2023-11-13 16:47:23 +0700

    .

--- simulation/script/py_init_vehicle.py
+++ simulation/script/py_init_vehicle.py
@@ -8,6 +8,7 @@ end = int(sys.argv[1]) # Read parameter from command
 # Insertion rate is defined number of vehicles in SUMO
 insertionRate = int(sys.argv[2])
 times = int(sys.argv[3])
+seed = int(sys.argv[4])
 
 # Define path to 
 # SUMO_HOME = "/home/parallel_user/sumo/"
@@ -18,7 +19,7 @@ PATH_ROUTE = "../sumo/route.rou.xml"
 
 # command1 = PATH_RANDOMTRIP + " -n" + PATH_NET + " -r " + PATH_ROUTE +" -b " + str(begin) +  " -e " + str(end) + " -p " + str(period) + " -l " 
 
-command1 = PATH_RANDOMTRIP + " -n" + PATH_NET + " -r " + PATH_ROUTE +" -b " + str(begin) +  " -e " + str(end) + " --insertion-rate " +  str(insertionRate) + " -l -s 45" 
+command1 = PATH_RANDOMTRIP + " -n" + PATH_NET + " -r " + PATH_ROUTE +" -b " + str(begin) +  " -e " + str(end) + " --insertion-rate " +  str(insertionRate) + " -l -s 46"
 
 # python3 my_random_trips.py -n net.net.xml -r route.rou.xml -b 0 -e 10 --insertion-rate 2 > my_random_trips.txt
 os.system(command1)
