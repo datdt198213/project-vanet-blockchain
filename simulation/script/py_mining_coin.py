5e9fb099426b496d75c11767ee545f690c996240
datdt198213 <dangdat222@gmail.com>
2023-11-21 22:42:59 +0700

    .

--- simulation/script/py_mining_coin.py
+++ simulation/script/py_mining_coin.py
@@ -1,40 +1,33 @@
 import os
+import sys
 
 totalTime = 36000
-times = 1
-insertionRate = 90
+insertionRate = 108
+distance = 1000
 
-# number of vehicles 100, 200, 300, 400, 500
-for i in range(1, 22):
-    numVehicles = insertionRate # number of vehicle
-    distance = 1000
-    command = "node JSAddHeader.js " + str(numVehicles)
-    os.system(command)
+command = "node JSAddHeader.js " + str(insertionRate)
+os.system(command)
 
-    # Distance 1km, 1.5km, 2km
-    for k in range(1, 4):
-        timeRound = 3600
-        # Timeslot 1h, 2h, 3h, 4h
-        for j in range(1, 5):
-            b = 0
-            e = timeRound
-            # Loop TimeSlot 5 times
-            while(1):
-                if (b < totalTime): 
-                    if (e > totalTime):
-                        e = totalTime
-                    command = "node JSMinerCoin.js " + str(timeRound) + " " + str(b) + " " + str(e) + " " + str(distance) + " " + str(times) + " " + str(numVehicles) + " " + str(totalTime)
-                    os.system(command)
-                    b += timeRound
-                    e += timeRound
-                else:
-                    print("WARNING: Time end at " + str(totalTime))
-                    break
-            timeRound += 3600
-
-        distance += 500
-        
-    times += 1
-    insertionRate += 1
+# Distance 1km, 1.5km, 2km
+for k in range(1, 4):
+    timeRound = 3600
+    # Timeslot 1h, 2h, 3h, 4h
+    for j in range(1, 5):
+        b = 0
+        e = timeRound
+        # Loop TimeSlot 5 times
+        while(1):
+            if (b < totalTime): 
+                if (e > totalTime):
+                    e = totalTime
+                command = "node JSMinerCoin.js " + str(timeRound) + " " + str(b) + " " + str(e) + " " + str(distance) + " " + str(insertionRate) + " " + str(totalTime)
+                os.system(command)
+                b += timeRound
+                e += timeRound
+            else:
+                print("WARNING: Time end at " + str(totalTime))
+                break
+        timeRound += 3600
+    distance += 500
 
    
