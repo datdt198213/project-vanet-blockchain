3d60d5fcd9819b0052d56df7e8ffba9c0c40f14f
datdt198213 <dangdat222@gmail.com>
2023-11-26 16:13:01 +0700

    .

--- simulation/script/py_main.py
+++ simulation/script/py_main.py
@@ -1,9 +1,9 @@
 import os
 
 totalTime = 36000
-insertionRate = 410
+insertionRate = 490
 
-for i in range(1, 2):
+for i in range(1, 22):
     # number of vehicles 90=>110, 190=>210, 290=>310, 390=>410, 490=>510
 
     # # init data of simulation   
