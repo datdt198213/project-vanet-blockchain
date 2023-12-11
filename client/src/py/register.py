a576466fdab83c13a2e50bce701c992c3ac864b1
datdt198213 <dangdat222@gmail.com>
2023-12-11 16:56:31 +0700

    update api register

--- client/src/py/register.py
+++ client/src/py/register.py
@@ -1,8 +1,9 @@
 import os
 import binascii
-from iroha import IrohaCrypto
-from iroha import Iroha, IrohaGrpc
-from iroha import primitive_pb2
+from iroha import Iroha, IrohaGrpc, IrohaCrypto, primitive_pb2
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
 
 IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '127.0.0.1')
 IROHA_PORT = os.getenv('IROHA_PORT', '50051')
@@ -48,6 +49,7 @@ def register(account, domain, public_key):
     ])
     IrohaCrypto.sign_transaction(tx, ADMIN_PRIVATE_KEY)
     send_transaction_and_print_status(tx)
+    print("Create an account successfully!")
 
 # Tạo cặp khóa public và private key ngẫu nhiên cho người dùng
 def generate_key():
@@ -65,12 +67,33 @@ def store_key(account_name, domain_id, private_key, public_key):
     with open(pub_file, 'w') as file:
         file.write(public_key.decode('utf-8'))
 
-def main():
-    account_name = "dat9"
-    domain_id = "test"
+# def main():
+#     account_name = "dat9"
+#     domain_id = "test"
+#     private_key, public_key = generate_key()
+#     register(account_name, domain_id, public_key)
+#     store_key(account_name, domain_id, private_key, public_key)
+
+# if __name__ == "__main__":
+#     main()
+
+@app.route('/register', methods=['POST'])   
+def register_account():
+    data = request.get_json()
+    account_name = data.get('account_name')
+    domain_id = data.get('domain_id')
+
     private_key, public_key = generate_key()
     register(account_name, domain_id, public_key)
     store_key(account_name, domain_id, private_key, public_key)
 
+    response = {
+        'status': 'success',
+        'message': f'Account {account_name}@{domain_id} registered successfully.'
+    }
+    return jsonify(response)
+
+
 if __name__ == "__main__":
-    main()
+    # app.run(host='0.0.0.0', port=5151)
+    pass
