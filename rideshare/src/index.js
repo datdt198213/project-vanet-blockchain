49042feeb32dbf9416f68f88f3954eefaf300fc4
datdt198213 <dangdat222@gmail.com>
2024-04-21 13:03:28 +0700

    Fix: react strict mode

--- rideshare/src/index.js
+++ rideshare/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
