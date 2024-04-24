7f20786d8ddf01f9407a07312befd2fb59a1cd81
datdt198213 <dangdat222@gmail.com>
2024-04-24 16:28:55 +0700

    Done create ride

---
+++ rideshare/src/component/CreateRequest.js
@@ -0,0 +1,9 @@
+
+
+const CreateRequest = () => {
+    return(<div>
+        Hello
+    </div>);
+}
+
+export default CreateRequest;
---
+++ rideshare/src/component/News.js
@@ -0,0 +1,5 @@
+const News = () => {
+    return (<h1>News PAGE</h1>)
+}
+
+export default News;
